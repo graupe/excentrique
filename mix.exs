efd03ad9fbd1d3d540d72d0cd44908f629d771c4
Stefan Schulze <stefan.andre.schulze@posteo.de>
2026-01-04 14:42:46 +0100

    Bump version to 1.1.1
     - Added missing support for guards in combination with `\\` in a match `<-`
     - Removed residue of old project name

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Excentrique.MixProject do
   def project do
     [
       app: :excentrique,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
